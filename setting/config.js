81fe3a1a3e5afa2d04f184e7d16423da771b8068
Bellfroutcs <87676463+Bellfroutcs@users.noreply.github.com>
2023-04-21 14:40:12 +0700

    Update config.js

--- setting/config.js
+++ setting/config.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-global.owner = ["6285742632270"];
-global.author = "RezaDevv";
+global.owner = ["6283816797646","62838167976460"];
+global.author = "NdaDev";
 global.packname = "Push-Kontak";
 global.sessionName = "Bot-Session";
 global.versionbot = "1.0.0";
